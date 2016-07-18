0db423464c40671ec19f21d41eb48a1ee7c9c729
DogNul <DogEsh@yandex.ru>
2016-07-19 00:37:28 +0700

    1.0.6

---
+++ Assets/Scene/Game/ButtonPlay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Mavirea.Scene.Game
+{
+    public class ButtonPlay : MonoBehaviour
+    {
+        public void SwitchButton()
+        {
+            GameObject par = gameObject.transform.parent.gameObject;
+            ButtonPause p = par.GetComponentInChildren<ButtonPause>();
+            GameObject g = p.gameObject;
+            g.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
