db7a83ce6533d4d5997c4824c2cf2cbd71147988
azazaza <DogEsh@yandex.ru>
2016-07-18 18:04:30 +0600

    1.0.3

--- Assets/Scene/Menu/ButtonPlay.cs
+++ Assets/Scene/Menu/ButtonPlay.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 using Mavirea.Command;
 
 namespace Mavirea.Scene.Menu
@@ -11,8 +12,13 @@ namespace Mavirea.Scene.Menu
     {
         public void PlayGame()
         {
+            gameObject.GetComponent<AudioSource>().Play();
             ICommand cmd = new CommandChangeScene(HelperSceneID.Game);
             SceneManager.GetScenario().SetCommand(cmd);
         }
+        //public void NNN()
+        //{
+        //    transform.GetComponent<Button>().interactable = false;
+        //}
     }
 }
